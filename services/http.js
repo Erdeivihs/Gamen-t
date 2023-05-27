889786fa8c598d0839df237ccfb1d3310a5d0dea
drakedemon <davidpalop2000@gmail.com>
2023-05-27 20:36:30 +0200

    Cosas

--- services/http.js
+++ services/http.js
@@ -9,37 +9,29 @@ const headers = {
 };
 
 
-
-///////////
-////////// Per a les peticions normals a dades de la base de dades 
-//////////
 async function supaRequest(url,method,headers,body){
     let response = await fetch(url,{
         method,
         headers,
-        body: JSON.stringify(body)  // En cas d'enviar dades per post, put patch... 
+        body: JSON.stringify(body)  
     });
-    if(response.status >=200 && response.status <=300){ // En cas d'error en el servidor
-        if(response.headers.get("content-type")){ // Si retorna un JSON
+    if(response.status >=200 && response.status <=300){ 
+        if(response.headers.get("content-type")){ 
             return await response.json();
         }
-        return {}; // Si no contesta res no té content-type i cal retornar un objecte buit per a ser coherent en l'eixida.
+        return {}; 
     }
     else{
-        return Promise.reject(await response.json()); // En cas de problemes en el servidor retornen un reject. 
+        return Promise.reject(await response.json()); 
     }
 }
 
 
-///////////
-////////// Per a les peticions a coses del storage
-////////// Cal un header diferent i tractar l'eixida de manera correcta.
-//////////
 async function fileRequest(url,body,token){
     const headersFile = {
         "apiKey": SUPABASE_KEY,
         "Authorization" :`Bearer ${token}`,
-        "x-upsert": true  // Necessari per a sobreescriure
+        "x-upsert": true
     }; 
     let response = await fetch(`${urlBase}${url}`,{
         method: 'POST',
@@ -48,8 +40,8 @@ async function fileRequest(url,body,token){
     });
     if(response.status >=200 && response.status <=300){
         if(response.headers.get("content-type")){
-            let datos = await response.json(); // Retorna un json amb la ruta relativa. 
-            datos.urlAvatar = `${urlBase}${url}`; // El que 
+            let datos = await response.json();  
+            datos.urlAvatar = `${urlBase}${url}`;  
             return datos;
         }
         return {};
@@ -101,41 +93,6 @@ async function logoutSupabase(token){
     return data;
 }
 
-async function recoverPasswordSupabase(email){
-    let url = `${urlBase}/auth/v1/recover`;
-    let headersAux = {...headers};
-    let data = await supaRequest(url,'post',headersAux,{email});
-    return data;
-}
-
-async function getData(URI,token){
-    let url = `${urlBase}/rest/v1/${URI}`;
-    let headersAux = {...headers, "Authorization" :"Bearer "+token};
-    let data = await supaRequest(url,'get',headersAux);
-    console.log(data);
-    return data;
-}
-
-async function updateData(URI,token,data){
-    let url = `${urlBase}/rest/v1/${URI}`;
-    let headersAux = {...headers, 
-        "Authorization" :"Bearer "+token,
-        "Prefer" : "return=representation"
-    };
-    let response = await supaRequest(url,'PATCH',headersAux,data);
-    return response;
-}
-
-async function createData(URI,token,data){
-    let url = `${urlBase}/rest/v1/${URI}`;
-    let headersAux = {...headers, 
-        "Authorization" :"Bearer "+token,
-        "Prefer" : "return=representation"
-    };
-    let response = await supaRequest(url,'post',headersAux,data);
-    return response;
-}
-
 async function getFiltro(URI,token){
     let url = `${urlBase}/rest/v1/${URI}`;
     let headersAux = {...headers, "Authorization" :"Bearer "+token};
@@ -178,33 +135,3 @@ async function getButtonValue(URI, token) {
     let response = await supaRequest(url,'get', headersAux);
     return response;
 }
-
-//async function getEliminar(tabla, idToDelete,token) {
-  //  let url = `${urlBase}/rest/v1/${tabla}?id=eq.${idToDelete}`;
-    //let headersAux = {...headers, "Authorization" :"Bearer "+token};
-    //let data = await supaRequest(url,'delete',headersAux);
-    //switch (tabla) {
-    //    case "indie":
-    //        window.location.hash = '#/indie';
-    //        router(window.location.hash);
-    //    break;
-    //
-    //    case "shooter":
-    //        window.location.hash = '#/shooter';
-    //        router(window.location.hash);
-    //    break;
-    //    case "plataformeo":
-    //        window.location.hash = '#/plataformeo';
-    //        router(window.location.hash);
-    //    break;
-    //    case "deportes":
-    //        window.location.hash = '#/deportes';
-    //        router(window.location.hash);
-    //    break;
-    //    case "carreras":
-    //        window.location.hash = '#/carreras';
-    //        router(window.location.hash);
-    //    break;
-   // }
-  //  return data;
- // }
