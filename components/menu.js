2b8521711168efb005d6cdcd4adb94ef607798fb
drakedemon <davidpalop2000@gmail.com>
2023-05-28 18:17:39 +0200

    Nuevo navbar

--- components/menu.js
+++ components/menu.js
@@ -8,59 +8,34 @@ class Menu {
   rendermenu() {
     let div = document.querySelector("#principal");
     div.innerHTML = `
-    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#/menu">Gamen't</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Shooter)">Shooter</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(ARPG)">ARPG</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Strategy)">Strategy</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(MMORPG)">MMORPG</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Fighting)">Fighting</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Battle Royale)">Battle Royale</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#/games(MOBA)">MOBA</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Card Game)">Card Game</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Sports)">Sports</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(MMO)">MMO</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Racing)">Racing</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#/games(Fantasy)">Fantasy</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/games(Social)">Social</a>
-          </li>        
-        </ul>
-          <a href="#/perfil" class="btn btn-outline-danger" type="button">Perfil</a>
-          <button id="logout" class="btn btn-outline-danger" type="button">LogOut</button>
-      </div>
-    </div>
-  </nav>
+    <link rel="stylesheet" type="text/css" href="./assets/css/menu.css"> 
+    <body>
+        <header class="header">
+            <div class="logo">
+                <a href="#/menu"><img src="../assets/img/favicon-removebg-preview.png" alt="Logo de la marca"></a>
+            </div>
+            <nav>
+               <ul class="nav-links">
+                    <li><a href="#/games(Shooter)">Shooter</a></li>
+                    <li><a href="#/games(ARPG)">ARPG</a></li>
+                    <li><a href="#/games(Strategy)">Strategy</a></li>
+                    <li><a href="#/games(MMORPG)">MMORPG</a></li>
+                    <li><a href="#/games(Fighting)">Fighting</a></li>
+                    <li><a href="#/games(Battle Royale)">Battle Royale</a></li>
+                    <li><a href="#/games(MOBA)">MOBA</a></li>
+                    <li><a href="#/games(Card Game)">Card Game</a></li>
+                    <li><a href="#/games(Sports)">Sports</a></li>
+                    <li><a href="#/games(MMO)">MMO</a></li>
+                    <li><a href="#/games(Racing)">Racing</a></li>
+                    <li><a href="#/games(Fantasy)">Fantasy</a></li>
+                    <li><a href="#/games(Social)">Social</a></li>
+               </ul>            
+            </nav>
+            <a class="btn" href="#/perfil"><button>Perfil</button></a>
+            <a id="logout" class="btn" href="#"><button>LogOut</button></a>
+        </header>
+        <div id="contenido"></div>
+    </body>
 
   <div id="contenido"></div>
     `;
@@ -68,3 +43,4 @@ class Menu {
         document.querySelector("#logout").addEventListener("click", logout);
   }
 }
+
